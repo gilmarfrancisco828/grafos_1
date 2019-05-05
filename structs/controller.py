a23465b164b052c3542a14a0ec050afa7782dbb4
luciano_s <lcsjr70@gmail.com>
2019-05-05 14:17:44 -0300

    Controller class documented

--- structs/controller.py
+++ structs/controller.py
@@ -1,28 +1,33 @@
 #coding utf-8
 
-from adj_list import AdjList
 from graph import Type as tp
+from adj_list import AdjList
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from file_graph import File
 
 
 class Controller(object):
+    "A classe controller, seguindo o padrão MVC, será a responsável por fazer o interfaceamento entre \no usuário (cliente main) e a implementação das estruturas e respectivos métodos."
     _adjg     = None
     _adjdg    = None
     _matrixg  = None
     _matrixdg = None
 
     def show_folders(self):
+        "método para procurar o arquivo que contém o grafo ou dígrafo"
         Tk().withdraw()
         filename = askopenfilename()
         self._arq = File()
         File.read_file(filename)
+   
 
     def close_file(self):
+        "método para fechar o arquivo do grafo atual"
         File.close_file()
 
     def show_menu(self):
+        "método para ter acesso as opções que o programa oferece"
         print('S - Sair\n'
               '1 - Escolher representação computacional\n'
               '2 - Busca em Profundidade\n'
@@ -36,10 +41,44 @@ class Controller(object):
         op = str(input('Digite o valor associado a opção escolhida: '))
         return op
 
+    def select_data_structure(self):
+        "metodo para escolher a representação computacional que será usada para o grafo"
+        print('1 - Lista de Adjacência\n'
+              '2 - Matriz de Adjacência\n')
+        return str(input('Digite o número referente a escolha: '))
+
     def get_vertexes(self):
         self._vertexes = File.get_vertexes()
-        print(*l)
+        
 
     def get_edges(self):
         self._edges = File.get_edges()
         
+    
+    def DFS(self):
+        "método para realizar o algoritmo de Depth-First Search"
+        pass
+    
+    def BFS(self):
+        "método referente ao algoritmo Breadth-First Search"
+        pass
+    
+    def caminho_entre_vertex(self):
+        "método para verificar se existe caminho entre dois vértices"
+        pass
+
+    def is_conected(self):
+        "método para verificar se um grafo é conexo"
+        pass
+    
+    def prim(self, s):
+        "método que utiliza o algoritmo de prim para encontrar uma árvore geradora mínima"
+        pass
+    
+    def dijkstra(self, s):
+        "método que utiliza o algoritmo de Dijkstra para encontrar o caminho mínimo\n de um vértice para todos os outros"
+        pass
+    
+    def bellman_ford(self):
+        "método que utiliza o algoritmo de Bellman-Ford para encontrar o caminho mínimo\n de um vértice para todos os outros"
+        pass
--- structs/main.py
+++ structs/main.py
@@ -2,20 +2,41 @@
 
 from controller import Controller
 
-
-
 def main():
 
     ctrl = Controller()
     ctrl.show_folders()
-    # while True:
-    #     op = ctrl.show_menu()
-    #     if op.lower() == 's':
-    #         exit()
+    #help(Controller)
+    while True:
+        op = ctrl.show_menu()
+        if op.lower() == 's':
+            exit()
+        elif int(op) not in list(range(1,10)):
+            print('Opção inválida!')
+        else:
+            if op == '1':
+                ctrl.select_data_structure()
+            elif op == '2':
+                ctrl.DFS()
+            elif op == '3':
+                ctrl.BFS()
+            elif op == '4':
+                ctrl.caminho_entre_vertex()
+            elif op == '5':
+                ctrl.is_conected()
+            elif op == '6':
+                ctrl.prim()
+            elif op == '7':
+                ctrl.kruskal()
+            elif op == '8':
+                ctrl.dijkstra()
+            elif op == '9':
+                ctrl.bellman_ford()
+            
+                
     
-    ctrl.get_vertexes()
-    ctrl.get_edges()
-
+    
+    
 
 if __name__ == '__main__':
     main()
