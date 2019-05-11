9f49f101dd947112f4aaee69c573da2933477b86
Gilmar Francisco <gilmarfrancisco828@gmail.com>
2019-05-11 12:51:08 -0300

    Add Kruskal and Bellman Ford

---
+++ algorithms/bellman_ford.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from structs.adj_list import AdjList
+from structs.adj_mat import AdjMatrix
+from structs.graph import Graph
+
+
+class BellmanFordAux(object):
+    def __init__(self, G: Graph, s):
+        self.d = defaultdict(dict)
+        self.pi = None
+        for i in G.vertexes:
+            self.d[i] = float('inf')
+            self.pi[i] = None
+        self.d[s] = 0
+
+
+def relax(aux: BellmanFordAux, u, v, w: int):
+    if aux.d[v] > (aux.d[u]+w):
+        aux.d[v] = aux.d[u] + w
+        aux.pi[v] = u
+
+
+def BELLMAN_FORD(G: Graph, s):
+    aux = BellmanFordAux(G, s)
+
+    for u in G.vertexes:
+        if isinstance(G, AdjMatrix):
+            for v in G.vertexes[u]:
+                relax(aux, u, v, G.get_value(u, v))
+        else:
+            cur = graph.vertexes[u]
+            while cur is not None:
+                relax(aux, u, cur.get_index(), cur.get_val())
+                cur = cur.get_prox()
+
+    for u in G.vertexes:
+        cur = graph.vertexes[u]
+        while cur is not None:
+            if aux.d[cur.get_index()] > (aux.d[u] + cur.get_val()):
+                return False
+            cur = cur.get_prox()
+
+    return True
+
---
+++ algorithms/kruskal.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from structs.adj_list import AdjList
+from structs.adj_mat import AdjMatrix
+from structs.graph import Graph
+
+
+def KRUSKAL(G: Graph):
+    """Retorna a árvore geradora mínima de um grafo
+    """
+    Al = []
+    X = []
+    setV = defaultdict(dict)
+    # get all the edges
+    for u in G.vertexes:
+        setV[u] = u
+        if isinstance(G, AdjList):
+            cur = G.vertexes[u]
+            while cur.get_next() is not None:
+                Al.append((u, cur.get_index(), cur.get_val()))
+                cur = cur.get_prox()
+        else:
+            for v in G.vertexes[u]:
+                Al.append((u, v, G.get_value(u, v)))
+
+    # sort the list of vertices
+    Al = sorted(Al, key=lambda x: x[2])
+
+    for uv in Al:
+        u = setV[uv[0]]
+        v = setV[uv[1]]
+        if u != v:
+            X.append((uv[0], uv[1]))
+            for i in setV:
+                if setV[i] == v:
+                    setV[i] = u
+    return X
