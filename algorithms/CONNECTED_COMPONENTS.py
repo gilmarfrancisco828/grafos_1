a7aa168592a8247274f8792fec7e16b568d21c80
luciano_s <lcsjr70@gmail.com>
2019-06-30 21:40:04 -0300

    Printing out connected graphs

--- algorithms/CONNECTED_COMPONENTS.py
+++ algorithms/CONNECTED_COMPONENTS.py
@@ -1,11 +1,28 @@
 from structs.graph import Graph
 from algorithms.DFS import *
-
-
+from algorithms.PRINT_GRAPH import *
+import random
 
 def connected_components(G: Graph):
     res = DFS(G)
+    random.seed()
+
+    r = lambda: random.randint(0, 255)
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(25,15))
+
     roots = set()
     for v in res.r:
         roots.add(res.r[v])
+    hex_colors = {}
+    for i in roots:
+        hex_colors[i] = '#%02X%02X%02X' % (r(), r(), r())
+    colors = {
+        'roots': roots,
+        'hex': hex_colors,
+        'elements': res.r
+    }
+    
+
+    PRINT_GRAPH(G, axes,'Componentes Conexas',  colors=colors,colors_fun=get_colors_components)
+    plt.show()
     return roots, res.r
--- test/connected_components_test.py
+++ test/connected_components_test.py
@@ -4,10 +4,11 @@ from algorithms.CONNECTED_COMPONENTS import *
 from test.helper_leitura import *
 # contents = read_file("test/test_files/01_vazio.txt")
 # contents = read_file('test/test_files/02_unitario.txt')
-contents = read_file("test/16_componentes_conectadas_I_grafo.txt")
-# contents = read_file("test/16_componentes_conectadas_II_grafo.txt")
+# contents = read_file("test/16_componentes_conectadas_I_grafo.txt")
+contents = read_file("test/16_componentes_conectadas_II_grafo.txt")
 # ax = axes.flatten()
 graph = generate_graph(contents, False, False, True)
+# graph = generate_graph(contents, True, False, True)
 # print(graph.vertexes)
 # print(res.f)
 res = connected_components(graph) 
